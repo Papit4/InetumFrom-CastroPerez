1e83ebda5cb72eada629383698c0382743457353
Enrique <castroperezenrique472@gmail.com>
2026-03-19 09:43:03 -0500

    Carpetas Src y configuracion Playwright cucumber

---
+++ cucumber.js
@@ -0,0 +1,15 @@
+module.exports = {
+  default: {
+    require: ['src/steps/**/*.ts', 'src/support/**/*.ts'],
+    requireModule: ['ts-node/register'],
+    format: [
+      'progress-bar',
+      'html:reports/cucumber-report.html',
+      'json:reports/cucumber-report.json'
+    ],
+    formatOptions: {
+      snippetInterface: 'async-await'
+    },
+    publishQuiet: true
+  }
+};
---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/features',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://www.saucedemo.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
